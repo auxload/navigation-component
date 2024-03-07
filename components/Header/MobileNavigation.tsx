74e0334a6db8de1132b27e968e00693f77f120ef
auxload <gd69435@gmail.com>
2024-03-07 21:40:41 +0200

    v0

--- components/Header/MobileNavigation.tsx
+++ components/Header/MobileNavigation.tsx
@@ -1,10 +1,6 @@
 "use client";
 import React from "react";
-import {
-  Sheet,
-  SheetContent,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { MenuIcon } from "lucide-react";
 import { Button } from "../ui/button";
 import {
@@ -23,17 +19,19 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
-const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-10 w-max items-center  justify-center rounded-md     text-sm font-medium transition-colors hover:underline X hover:text-accent-foreground focus:underline focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:underline "
-);
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { cva } from "class-variance-authority";
 import { data } from "./navConfig";
+
+
+const navigationMenuTriggerStyle = cva(
+  "group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50"
+);
 const Navigation = () => {
   return (
     <NavigationMenu className="flex-none block max-w-none">
-      <NavigationMenuList className="flex-none block space-x-0">
+      <NavigationMenuList className="flex-none block space-x-0 space-y-1">
         {data.map((obj) => {
           if (obj.subMenu) {
             return (
@@ -60,7 +58,7 @@ export const LinkItem = ({ path, label }: { path: string; label: string }) => {
         <NavigationMenuLink
           className={cn(
             navigationMenuTriggerStyle(),
-            "w-full flex  justify-start py-6 rounded-none "
+            "w-full flex  justify-start py-6  "
           )}
         >
           {label}
@@ -82,7 +80,7 @@ export const LinkItemVariation = ({
         <NavigationMenuLink
           className={cn(
             navigationMenuTriggerStyle(),
-            "w-full flex hover:bg-transparent  justify-start py-6 rounded-none "
+            "w-full flex   justify-start py-6  "
           )}
         >
           {label}
@@ -106,14 +104,14 @@ export const LinkItemMenu = ({
           <AccordionTrigger
             className={cn(
               navigationMenuTriggerStyle(),
-              "justify-between rounded-none py-6"
+              "justify-between hover:no-underline py-6"
             )}
           >
             {label}
           </AccordionTrigger>
-          <AccordionContent className="flex   gap-4">
+          <AccordionContent className="flex  gap-4 py-3">
             <span className="w-[2px]  bg-accent "></span>
-            <NavigationMenuList className="flex-none block space-x-0">
+            <NavigationMenuList className="flex-none block space-x-0 space-y-1 ">
               {menu.map((item) => {
                 return (
                   <LinkItemVariation
@@ -130,7 +128,6 @@ export const LinkItemMenu = ({
     </NavigationMenuItem>
   );
 };
-
 
 const MobileNavigation = () => {
   return (
